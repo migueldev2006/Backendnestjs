be541a7de040776102f8b519ce5adf83ec7b86e7
migueldev2006 <gironorozcoluismiguel3@gmail.com>
2025-05-12 08:59:08 -0500

    entity areas faltan relaciones

--- src/areas/entities/area.entity.ts
+++ src/areas/entities/area.entity.ts
@@ -1,1 +1,24 @@
-export class Area {}
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name:'areas'})
+
+export class Area {
+  @PrimaryGeneratedColumn('increment')
+  id_area: number;
+
+  @Column({ type: 'varchar', length: 20 })
+  nombre: string;
+
+  @Column({ type: 'boolean', default: true })
+  estado: boolean;
+
+  @Column({ type: 'timestamp' })
+  created_at: Date;
+
+  @Column({ type: 'timestamp' })
+  updated_at: Date;
+
+  @Column('text', { unique: true })
+  slug: string;
+
+}
