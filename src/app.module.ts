597b31caa586b79b0158780512631a453b2ac947
Yurgen2007 <yurgenpv@gmail.com>
2025-05-15 08:26:23 -0500

    cambios master

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,6 +43,7 @@ import { NotificacionesModule } from './notificaciones/notificaciones.module';
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
+      entities: [__dirname + '//*.entity{.ts,.js}'],
       autoLoadEntities: true,
       synchronize: true
     }),
